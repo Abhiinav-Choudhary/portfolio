1919e3ccea822f7012df1158de6c951972fba2f4
Abhinav Chaudhary <abhinavchaudhary30993@gmail.com>
2025-06-16 13:14:20 +0530

    .

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,29 +10,31 @@ function Home() {
     navigate('/contact');
   };
 
-  
-
   return (
     <>
       <Header />
       <div className="container">
+        {/* Left Side - Text Section */}
         <div className="intro">
           <div className="text">
             <h1>
               Hi, <br />
-              I'm <span className="title" >Abhinav</span><br />
+              I'm <span className="title">Abhinav</span><br />
               <span className="work">Web Developer</span>
             </h1>
           </div>
           <button className="btn" onClick={move}>Contact</button>
         </div>
 
-        <div className="profile-pic-wrapper">
-          <img
-            src="/abhisanket.jpg"
-            alt="Abhinav's Profile"
-            className="profile-pic"
-          />
+        {/* Right Side - Glass Profile Section */}
+        <div className="glass-wrapper">
+          <div className="profile-pic-wrapper">
+            <img
+              src="/abhisanket.jpg"
+              alt="Abhinav's Profile"
+              className="profile-pic"
+            />
+          </div>
         </div>
       </div>
     </>
